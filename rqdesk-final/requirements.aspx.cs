0b50a4f25006a58f8df22e592553c1965827ddc6
Santiago Serrano Márquez <santiago.serrano.marquez@hotmail.com>
2020-11-12 22:15:33 -0600

    Minor Fixes

--- rqdesk-final/requirements.aspx.cs
+++ rqdesk-final/requirements.aspx.cs
@@ -18,17 +18,14 @@ namespace rqdesk_final
             {
                 SqlCommand cmd = new SqlCommand("SELECT * FROM requirements", cn);
                 cn.Open();
-
                 SqlDataReader dr = cmd.ExecuteReader();
-
-                
                 if (dr.HasRows)
                 {
                     card.Append("<div class='card-columns'>");
                     while (dr.Read())
                     {
                         card.Append("<div class='card border-dark'>");
-                        card.Append("<div class='card-header'>" + dr[3] + "<button id=" + dr[0] + " type='button' class='btn btn-outline-secondary btn-sm open-modal-edit'>Editar</button></div>");
+                        card.Append("<div class='card-header'>" + dr[3] + "<button id='btnEditar" + dr[0] + "' value=" + dr[0] + " type='button' runat='server' class='btn btn-outline-secondary btn-sm open-modal-edit'>Editar</button> </div>");
                         card.Append("<div class='card-body'>");
                         card.Append("<h5 class='card-title'>" + dr[1] + "</h5>");
                         card.Append("<p class='card-text'>" + dr[2] + "</p>");
@@ -40,15 +37,14 @@ namespace rqdesk_final
                     }
                     card.Append("</div>");
                 }
-
                 else
                 {
                     card.Append("<div class='alert alert-light alert-responsive' role='alert'>Ups! Parece que no hay requerimientos, puedes añadir uno <a href='#' class='alert-link'>aquí</a>.</div>");
                 }
-
                 PlaceHolder1.Controls.Add(new Literal { Text = card.ToString() });  
-
                 cn.Close();
+
+                
             }
 
         }
